d475dda9c958fb850eb03f67306efbd145c99796
emilia <60214848+emilialei1@users.noreply.github.com>
2023-03-14 19:03:56 +0200

    Basic game mechanics

---
+++ DatabaseGame/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void LoadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- DatabaseGame/Assets/Scripts/SpacePress.cs
+++ DatabaseGame/Assets/Scripts/SpacePress.cs
@@ -7,17 +7,59 @@ public class SpacePress : MonoBehaviour
 {
     public Animator spaceAnim;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI endScoreText;
     public TextMeshProUGUI timerText;
+
+    public GameObject gameOverScreen;
+
+    private int score;
+    public bool isGameOn = false;
+    public bool isGameOver;
+    public float timeLeft = 30.0f;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGameOver == false)
         {
             spaceAnim.SetBool("isKeyDown", true);
+            UpdateScore(1);
+            isGameOn = true;
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) && isGameOver == false)
         {
             spaceAnim.SetBool("isKeyDown", false);
         }
+
+        if(isGameOn == true)
+        {
+            timerText.text = " " + Mathf.RoundToInt(timeLeft);
+
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                GameOver();
+            }
+        }
     }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = " " + score;
+        endScoreText.text = "Your score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        isGameOver = true;
+    }
+
+   /* public IEnumerator StartTimer()
+    {
+        isGameOn = true;
+        yield return new WaitForSeconds(30);
+        gameOverScreen.SetActive(true);
+    }*/
 }
